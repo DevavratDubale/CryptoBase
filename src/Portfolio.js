6ebb5f9eb5d72ddfd836c5d34fcc662704c3aca3
Umang Bhutoria <bhutoriaumang@gmail.com>
2022-02-27 21:41:52 +0530

    Improved responsivity of the website

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,10 +1,38 @@
 import sidebarImage from './assets/SidebarImage.png'
 import dpalt from './assets/DisplayPictureAlt.png'
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import { IconContext } from "react-icons/lib";
+import { GoTriangleUp,GoTriangleDown } from "react-icons/go";
+import { FaPlus } from 'react-icons/fa';
+
+const ColorButtonA = styled(Button)(({ theme }) => ({
+    color: "white",
+    backgroundImage: "linear-gradient(to right,#7716A5, #C1A9E8)",
+    marginLeft: "1.5vw",
+    fontWeight: "500",
+    fontSize: "min(1.5vh,1.5vw)"
+}));
+
+const ColorButtonB = styled(Button)(({ theme }) => ({
+    color: "white",
+    backgroundImage: "linear-gradient(to right,#645087, #C1A9E8)",
+    fontWeight: "500",
+    fontSize: "min(1.5vh,1.5vw)"
+}));
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const Portfolio = () => {
-    const balance = 1474.91;
+
+
+    const balance = (1474.9109).toFixed(2);
+    const changeInBalance = (301.93).toFixed(2);
     return(
-        <div className="sidebar">
+        <div className="total-area">
+            <div className="sidebar">
             <div className="portfolio-info">
                 <div className="useless-space" style={{height:"10vh"}}></div>
                 <div className="portfolio">
@@ -13,13 +41,42 @@ const Portfolio = () => {
                   </div>
                   <div className="portfolio-text">
                       <div className="portfolio-text-up">My Portfolio</div>
-                      <div className="portfolio-text-down">${balance}</div>
+                      <div className="portfolio-text-down">${numberWithCommas(balance)}</div>
                   </div>
                 </div>
             </div>
             <div className="sidebar-img">
                 <img src={sidebarImage} alt=""/>
             </div>
+        </div>
+        <div className="main-content">
+            <div className="main-content-heading">
+                <div className="main-content-heading-up">
+                    Current Balance
+                </div>
+                <div className="main-content-heading-down">
+                    ${numberWithCommas(balance)}
+                    <div className="buttonA">
+                    <ColorButtonA fullWidth={true} variant="contained" startIcon={(changeInBalance>0)?
+                        <IconContext.Provider value={{ className: "buttonA-icon" }}>
+                        <GoTriangleUp />
+                      </IconContext.Provider>
+                        :
+                        <IconContext.Provider value={{ className: "buttonA-icon" }}>
+                        <GoTriangleDown />
+                      </IconContext.Provider>
+                    }>{ ((changeInBalance/balance)*100).toFixed(2) }%</ColorButtonA>
+                    </div>
+                    <div className="buttonB">
+                    <ColorButtonB fullWidth= {true} variant="contained" startIcon={
+                        <IconContext.Provider value={{ className: "buttonB-icon" }}>
+                            <FaPlus />
+                        </IconContext.Provider>
+                    }>Add Crypto</ColorButtonB>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     );
 }
