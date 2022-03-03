5a762cc2afd8c986aac7a122f6d133959861ef8c
SuhaniBajaj-23 <suhani.bajaj2020@vitstudent.ac.in>
2022-03-03 23:58:45 +0530

    main page half done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from './HomePage';
 import NavBar from './NavBar';
+import Learn from './Learn';
 import Portfolio from './Portfolio';
 
 function App() {
   return (
     <Router>
-      <NavBar />
+      
       <div className="App">
         <Switch>
-          <Route path="/">
+        <Route exact path="/">
+        <NavBar />
+            <HomePage />
+          </Route>
+          <Route exact path="/learn">
+          <NavBar />
+            <Learn />
+          </Route>
+          <Route exact path="/portfolio">
+          <NavBar />
             <Portfolio />
           </Route>
+          
         </Switch>
       </div>
     </Router>
